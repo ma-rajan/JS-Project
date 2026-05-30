e6fbd50b1285bbe7e277cf802248f4b0b9bcf5c4
rajan <rajancdy14@gmail.com>
2026-05-30 16:59:11 +0545

    Food Recipe Book

---
+++ Food Recipe Book/script.js
@@ -0,0 +1,38 @@
+const momo=document.getElementById("momo");
+momo.addEventListener("click",event=>{
+    window.location.href="https://www.foodnetwork.com/recipes/nepali-momo-4635126.com";
+});
+const pizza=document.getElementById("pizza");
+pizza.addEventListener("click",event=>{
+    window.location.href="https://www.indianhealthyrecipes.com/pizza-recipe-make-pizza/";
+});
+const dalbhat=document.getElementById("dalbhat");
+dalbhat.addEventListener("click",event=>{
+    window.location.href="https://www.cookshideout.com/nepali-dal-bhat-recipe";
+});
+const salroti=document.getElementById("salroti");
+salroti.addEventListener("click",event=>{
+    window.location.href="https://www.pabskitchen.com/single-post/my-mom-s-recipe-sel-roti";
+});
+const choila=document.getElementById("choila");
+choila.addEventListener("click",event=>{
+    window.location.href="https://www.thetinytaster.com/choila-chicken-recipe-nepali-style-spicy-chicken/";
+});
+const thukpa=document.getElementById("thukpa");
+thukpa.addEventListener("click",event=>{
+    window.location.href="https://www.allrecipes.com/recipe/283775/vegetable-thukpa/";
+});
+const chatpat=document.getElementById("chatpat");
+chatpat.addEventListener("click",event=>{
+    window.location.href="https://spoonbun.com/wai-wai-chatpate/";
+});
+const panipuri=document.getElementById("panipuri");
+panipuri.addEventListener("click",event=>{
+    window.location.href="https://www.thegundruk.com/homemade-paanipuri-recipe/";
+});
+const kheer=document.getElementById("kheer");
+kheer.addEventListener("click",event=>{
+    window.location.href="https://www.indianhealthyrecipes.com/rice-kheer-recipe-chawal-ki-kheer/";
+});
+
+
