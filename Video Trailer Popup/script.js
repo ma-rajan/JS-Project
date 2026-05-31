fca8947bedd8a434884c9d029de2094afd7d6a48
rajan <rajancdy14@gmail.com>
2026-05-31 20:42:34 +0545

    Marvel Move Trailers with information's

---
+++ Video Trailer Popup/script.js
@@ -0,0 +1,37 @@
+const btnwatch1=document.getElementById("btnwatch1");
+btnwatch1.addEventListener("click",event=>{
+    window.open("Video/1.mp4","_blank");
+});
+
+const btnwatch2=document.getElementById("btnwatch2");
+btnwatch2.addEventListener("click",event=>{
+    window.open("Video/2.mp4","_blank");
+});
+const btnwatch3=document.getElementById("btnwatch3");
+btnwatch3.addEventListener("click",event=>{
+    window.open("Video/3.mp4","_blank");
+});
+const btnwatch4=document.getElementById("btnwatch4");
+btnwatch4.addEventListener("click",event=>{
+    window.open("Video/4.mp4","_blank");
+});
+const btnwatch5=document.getElementById("btnwatch5");
+btnwatch5.addEventListener("click",event=>{
+    window.open("Video/5.mp4","_blank");
+});
+const btnwatch6=document.getElementById("btnwatch6");
+btnwatch6.addEventListener("click",event=>{
+    window.open("Video/6.mp4","_blank");
+});
+const btnwatch7=document.getElementById("btnwatch7");
+btnwatch7.addEventListener("click",event=>{
+    window.open("Video/7.mp4","_blank");
+});
+const btnwatch8=document.getElementById("btnwatch8");
+btnwatch8.addEventListener("click",event=>{
+    window.open("Video/8.mp4","_blank");
+});
+const btnwatch9=document.getElementById("btnwatch9");
+btnwatch9.addEventListener("click",event=>{
+    window.open("Video/9.mp4","_blank");
+});
